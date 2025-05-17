21d801f48424c178017b29e1d79ec12c94c23831
Drei Abmab <dreiabmab@gmail.com>
2025-05-17 20:49:29 +0800

    Updated Auth Page

--- src/components/website/Auth/Login.jsx
+++ src/components/website/Auth/Login.jsx
@@ -1,51 +1,53 @@
 import React, { use } from 'react'
 import { google_icon } from '../../../assets'
 
-const Login = ({toggleForms, navigateTo}) => {
-  
+const Login = ({ toggleForms, navigateTo }) => {
+
   return (
     <div className="form login">
       <div className="tag">
         <p>Sign In</p>
       </div>
 
-      <h1>
-        Welcome back — <br />
-        let’s get you smiling
-        <br />
-        again.
-      </h1>
+      <div className="wrapper_content">
+        <h1>
+          Welcome back — <br />
+          let’s get you smiling
+          <br />
+          again.
+        </h1>
 
-      <form>
-        <label>
-          USERNAME
-        </label>
-        <input type="text" />
-        <br />
+        <form>
+          <label>
+            USERNAME
+          </label>
+          <input type="text" />
+          <br />
 
-        <label>
-          PASSWORD
-        </label>
-        <input type="password" />
-        <br />
+          <label>
+            PASSWORD
+          </label>
+          <input type="password" />
+          <br />
 
-        <button onClick={navigateTo}>LOGIN</button>
-      </form>
+          <button onClick={navigateTo}>LOGIN</button>
+        </form>
 
-      <a href="#">
-        {" "}
-        Forgot password?{" "}
-        <u>
-          <b>Click Here</b>
-        </u>{" "}
-      </a>
+        <a href="#">
+          {" "}
+          Forgot password?{" "}
+          <u>
+            <b>Click Here</b>
+          </u>{" "}
+        </a>
 
-      <button className="google">
-        <img src={google_icon} alt="" /> <span>Sign In With Google</span>
-      </button>
+        <button className="google">
+          <img src={google_icon} alt="" /> <span>Sign In With Google</span>
+        </button>
 
+        <button onClick={toggleForms}>Don't Have an account? Register</button>
 
-      <button onClick={toggleForms}>Don't Have an account? Register</button>
+      </div>
     </div>
   )
 }
--- src/components/website/Auth/Register.jsx
+++ src/components/website/Auth/Register.jsx
@@ -1,44 +1,48 @@
 import React from 'react'
 import { google_icon } from '../../../assets'
 
-const Register = ({toggleForms, registerCompleted}) => {
+const Register = ({ toggleForms, registerCompleted }) => {
   return (
     <div className="form register">
       <div className="tag">
         <p>REGISTER</p>
       </div>
 
-      <h1>
-        book Your
-        <br />
-        appointment
-        <br />
-        within minutes.
-      </h1>
+      <div className="wrapper_content">
+        <h1>
+          book Your
+          <br />
+          appointment
+          <br />
+          within minutes.
+        </h1>
 
-      <form onSubmit={registerCompleted}>
-        <label>
-          USERNAME
-        </label>
-        <input type="text" />
-        <br />
+        <form onSubmit={registerCompleted}>
+          <label>
+            USERNAME
+          </label>
+          <input type="text" />
+          <br />
 
-        <label>
-          PASSWORD
-        </label>
-        <input type="password" />
-        <br />
+          <label>
+            PASSWORD
+          </label>
+          <input type="password" />
+          <br />
 
-        <label>
-          EMAIL
-        </label>
-        <input type="email" />
-        <br />
+          <label>
+            EMAIL
+          </label>
+          <input type="email" />
+          <br />
 
-        <button>Register</button>
-      </form>
-      <button className="google"><img src={google_icon} alt="" /> <span>Register With Google</span></button>
-      <button onClick={toggleForms}>All Ready Have An Account? Login</button>
+          <button>Register</button>
+        </form>
+
+        <button className="google"><img src={google_icon} alt="" /> <span>Register With Google</span></button>
+        <button onClick={toggleForms}>All Ready Have An Account? Login</button>
+
+      </div>
     </div>
   )
 }
